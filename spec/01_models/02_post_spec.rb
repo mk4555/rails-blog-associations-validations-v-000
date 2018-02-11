83273491bc1e1904149c72daf066dad81618c0e5
Min Kim <mk4555@nyu.edu>
2018-02-11 22:59:18 +0000

    automatically backed up by learn

--- spec/01_models/02_post_spec.rb
+++ spec/01_models/02_post_spec.rb
@@ -15,8 +15,8 @@ describe Post do
 
   context 'Post relationship to User and Tag' do
     let(:user) { User.create(name: 'Crookshanks') }
-    # let(:tag1) { Tag.create(name: 'adorable') }
-    # let(:tag2) { Tag.create(name: 'feline') }
+    let(:tag1) { Tag.create(name: 'adorable') }
+    let(:tag2) { Tag.create(name: 'feline') }
     let(:post) { Post.create(user_id: user.id, name: 'post', content: 'content') }
 
     before do
